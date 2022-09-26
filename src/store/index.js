e49b024c8596de81e0280ecbfd0c6daad0073cce
Marko Obradovic <marko.obradovic@bearingpoint.com>
2022-09-26 10:03:10 +0200

    - deleted app bar - added store state, mutations and actions for adding selected pokemons and remove them if unselected

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,30 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        pokemons: [],
+        selectedPokemons: [],
+    },
+    getters: {
+        selectedPokemons: state => state.selectedPokemons
+    },
+    mutations: {
+        ADD_POKEMON: (state, pokemon) => state.selectedPokemons.push(pokemon),
+        REMOVE_POKEMON: (state, pokemonId) => {
+            state.selectedPokemons.forEach((selected, index) => {
+                if (pokemonId === selected.id) {
+                    state.selectedPokemons.splice(1, index)
+                }
+            })
+        }
+    },
+    actions: {
+        addPokemon({commit}, pokemon) {
+            commit("ADD_POKEMON", pokemon);
+        },
+        removePokemon({commit}, pokemonId) {
+            commit("REMOVE_POKEMON", pokemonId)
+        }
+    },
+    modules: {}
 })
